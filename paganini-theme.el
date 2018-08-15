5c1c82030cb9e9fecd7d382cb2b60a6c5dafa1f8
onurtemizkan <onurtemizkan@gmail.com>
2018-08-15 22:14:07 +0300

    Revert "Clean unnecessary comments" - Fixes #1
    This reverts commit c7474dd275dca81e77542987c459b4baba1fe190.

--- paganini-theme.el
+++ paganini-theme.el
@@ -1,13 +1,13 @@
 ;;; paganini-theme.el --- A colorful, dark and warm theme.
 ;;
 ;;; Author: Onur Temizkan
-;;; Version: 0.12
+;;; Version: 0.11
 ;;
 ;;; Url: https://github.com/onurtemizkan/paganini
 ;;; Package-Requires: ((emacs "24.0"))
 ;;
 ;;; Commentary:
-;;  A colorful, dark and warm Emacs theme.
+;;  This theme is based on FireCode
 ;;
 ;;; Code:
 
@@ -20,10 +20,13 @@
 
 (custom-theme-set-faces
  'paganini
+ ;; basic theming.
+
  '(default ((t (:foreground "#F8F8F2" :background "#191919" ))))
  '(region ((t (:background "firebrick" :foreground "gainsboro"))))
  '(cursor ((t (:background "orange red"))))
 
+ ;; Temporary defaults
  '(linum                               ((t (:foreground "#464644"  :background "#2f2f2f" ))))
  '(fringe                              ((t (                       :background "#2f2f2f" ))))
 
@@ -60,14 +63,18 @@
  '(secondary-selection                 ((t (                       :background "#342858"                                               ))))
  '(cua-rectangle                       ((t (:foreground "#E0E4CC"  :background "#342858" ))))
 
+ ;; Magit hightlight
  '(magit-item-highlight                ((t (:foreground "white" :background "#1278A8" :inherit nil ))))
 
+ ;; flyspell-mode
  '(flyspell-incorrect                  ((t (:underline "#AA0000" :background nil :inherit nil ))))
  '(flyspell-duplicate                  ((t (:underline "#009945" :background nil :inherit nil ))))
 
+ ;; flymake-mode
  '(flymake-errline                     ((t (:underline "#AA0000" :background nil :inherit nil ))))
  '(flymake-warnline                    ((t (:underline "#009945" :background nil :inherit nil ))))
 
+ ;;git-gutter
  '(git-gutter:added                    ((t (:foreground "#609f60" :bold t))))
  '(git-gutter:modified                 ((t (:foreground "#3388cc" :bold t))))
  '(git-gutter:deleted                  ((t (:foreground "#cc3333" :bold t))))
@@ -79,13 +86,17 @@
  '(diff-changed                        ((t (:foreground "#3388cc"))))
  '(diff-hunk-header                    ((t (:background "#242130"))))
 
+ ;; ecb
  '(ecb-analyse-face ((t (:inherit ecb-default-highlight-face :background "firebrick"))))
  '(ecb-default-highlight-face ((t (:background "forest green"))))
 
+ ;; ensime
  '(ensime-implicit-highlight ((t (:underline (:color "pale green" :style wave)))))
 
+ ;; flycheck
  '(flycheck-error ((t (:underline "dark sea green"))))
 
+ ;; LaTeX
  '(font-latex-sectioning-0-face ((t (:inherit font-latex-sectioning-1-face))))
  '(font-latex-sectioning-1-face ((t (:inherit font-latex-sectioning-2-face))))
  '(font-latex-sectioning-2-face ((t (:inherit font-latex-sectioning-3-face))))
@@ -95,17 +106,23 @@
  '(font-latex-slide-title-face ((t (:inherit font-lock-type-face :weight bold))))
  '(font-latex-verbatim-face ((t (:foreground "burlywood"))))
 
+ ;; hl-line
  '(hl-line ((t (:background "#422E39"))))
 
+ ;; popup
  '(popup-face ((t (:background "gray5" :foreground "gainsboro"))))
  '(popup-tip-face ((t (:background "gray15" :foreground "lemon chiffon"))))
 
+ ;; scala
  '(scala-font-lock:var-face ((t (:background "orange" :foreground "black"))))
 
+ ;; paren-match
  '(show-paren-match ((t (:foreground "gainsboro" :slant oblique :weight ultra-bold))))
 
+ ;; vertical-border
  '(vertical-border ((((type x pm tty)) (:background "black" :foreground "dark slate gray"))))
 
+ ;; window-divider
  '(window-divider ((t (:foreground "dark green"))))
  '(window-divider-first-pixel ((t (:foreground "dark green"))))
  '(window-divider-last-pixel ((t (:foreground "medium sea green"))))
@@ -132,7 +149,9 @@
  '(font-lock-regexp-grouping-backslash ((t (:foreground "#3498db"))))
  '(font-lock-regexp-grouping-construct ((t (:foreground "#3498db"))))
 )
+ ;; End face definitions
 
+;;;###autoload
 (when load-file-name
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
